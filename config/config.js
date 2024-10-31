f9cc725d01b8cd4d94590d9722540b8e21d5eebf
Raul de Oliveira <raulzilla.dev@gmail.com>
2024-10-31 17:54:09 -0300

    fix: update config

--- config/config.js
+++ config/config.js
@@ -5,6 +5,7 @@ const config = {
     `⚡️ Utilize ${prefix} em todos comando`,
     `⚡️ Para ver quanto tempo falta para nossa viagem digite: ${prefix} viagem`,
     `⚡️ Para ver o rank de algum player no valorant, faça como esse exemplo: ${prefix} rank MDKS R3n3g4d3#2004`,
+    `⚡️ Para ver o a comp do clan nos mapas, digite: ${prefix} comps`,
   ],
   prefix,
 };
